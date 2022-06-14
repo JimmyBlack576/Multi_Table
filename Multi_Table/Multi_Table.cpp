0f4ebf84af247aecad16d8298dba95166a40e84d
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-14 19:11:42 +0300

    Добавьте файлы проекта.

---
+++ Multi_Table/Multi_Table.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите число для отображения таблицы умножения на него: \n";
+    int c, a;
+    cin >> a;
+    for (int i = 2; i <= 10; i++)
+    {
+        c = a * i;
+        cout << endl << a << " * " << i << " = " << c;
+    }
+    
+
+}
+
